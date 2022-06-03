0dca2002eac83ce2e87af0188287e03d2b5a9556
MalikaWeeroda <malika.k@weeroda.com>
2022-06-03 17:25:31 +0530

    Reordered the Tabs
    Reordered the Tabs

--- RY_Enquiry_Form/views.py
+++ RY_Enquiry_Form/views.py
@@ -1,8 +1,10 @@
+from asyncio.windows_events import NULL
 from django.shortcuts import redirect, render
 from psycopg2 import Date
 from .forms import Ry_En_Form, Ry_En_Header
 from .models import RY_Enquiry_Header, RY_Enquiry_Items
 import subprocess
+from django.db.models import Q
 
 
 def index(request):
@@ -15,13 +17,27 @@ def index(request):
     if vReg_no == None and vFlag == None:
         context = {}
         Unread_Data = RY_Enquiry_Header.objects.filter(Status='0')
-        Updated_Pending_ERP = RY_Enquiry_Header.objects.filter(Status='1')
-        Ready_For_Quotation = RY_Enquiry_Header.objects.filter(Status='2')
+        other_status = RY_Enquiry_Header.objects.filter(
+            ~Q(Status='0') & ~Q(Status='3'))
+        # Ready_For_Quotation = RY_Enquiry_Header.objects.filter(Status='2')
+        Req_Yarn_Price = RY_Enquiry_Header.objects.filter(Status='3')
 
         context['Unread_Data'] = Unread_Data
-        context['Updated_Pending_ERP'] = Updated_Pending_ERP
-        context['Ready_For_Quotation'] = Ready_For_Quotation
-        print(context)
+        context['other_status'] = other_status
+        # context['Ready_For_Quotation'] = Ready_For_Quotation
+        context['Req_Yarn_Price'] = Req_Yarn_Price
+
+        Count_Unr = len(Unread_Data)
+        Count_Upd = len(other_status)
+        # Count_Rfq = len(Ready_For_Quotation)
+        Count_RYP = len(Req_Yarn_Price)
+
+        context['Count_Unr'] = Count_Unr
+        context['Count_Upd'] = Count_Upd
+        # context['Count_Rfq'] = Count_Rfq
+        context['Count_RYP'] = Count_RYP
+
+        # print(Count_Unr, Count_Upd, Count_Rfq)
 
         return render(request, 'app/ryn.html', context)
 
@@ -43,7 +59,7 @@ def index(request):
             Marketing_Zone = item.Marketing_Zone
             Mill = item.Mill
             Customer = item.Customer
-            if item.Status == '2':
+            if item.Status >= '3':
                 vStatus = 'readonly'
 
     if len(data) != 0:
@@ -58,6 +74,9 @@ def index(request):
             UOM = item.UOM
             Quantity = item.Quantity
             Status = item.Status
+            Rate = item.Rate
+            Amount = item.Amount
+            Last_order = item.Last_order
 
             print("data from database Counts :", Counts)
 
@@ -81,8 +100,10 @@ def index(request):
             'Marketing_Zone': Marketing_Zone,
             'Mill': Mill,
             'Customer': Customer,
-            'Feild_Type': vStatus
-
+            'Feild_Type': vStatus,
+            'Rate': Rate,
+            'Amount': Amount,
+            'Last_order': Last_order
         }
     else:
         context = {'Error': 'No data found'
@@ -99,11 +120,16 @@ def update_book(request, data, vReg_no, data2):
 
         for item in data:
             vid = str(item.id)
+            vStatus = '1'
+            print("values from form", request.POST.get('Rate'+str(item.id)))
+            Rate = request.POST.get('Rate'+str(item.id))
+            if Rate != None:
+                vStatus = '4'
             RY_Enquiry_Items.objects.filter(id=vid).update(
-                Counts=request.POST.get('Counts'+str(item.id)), Quality=request.POST.get('Quality'+str(item.id)), Type=request.POST.get('YarnType'+str(item.id)), Blend=request.POST.get('Blend'+str(item.id)), Shade=request.POST.get('Shade'+str(item.id)), Depth=request.POST.get('Depth'+str(item.id)), UOM=request.POST.get('UOM'+str(item.id)), Quantity=request.POST.get('Quantity'+str(item.id)), Status=1)
+                Counts=request.POST.get('Counts'+str(item.id)), Quality=request.POST.get('Quality'+str(item.id)), Type=request.POST.get('YarnType'+str(item.id)), Blend=request.POST.get('Blend'+str(item.id)), Shade=request.POST.get('Shade'+str(item.id)), Depth=request.POST.get('Depth'+str(item.id)), UOM=request.POST.get('UOM'+str(item.id)), Quantity=request.POST.get('Quantity'+str(item.id)), Rate=request.POST.get('Rate'+str(item.id)), Amount=request.POST.get('Amount'+str(item.id)), Last_order=request.POST.get('Last_order'+str(item.id)), Status=vStatus)
 
         RY_Enquiry_Header.objects.filter(Reg_no=vReg_no).update(
-            Mill=request.POST.get('Mill'), Date=request.POST.get('Date'), Mill_Rep=request.POST.get('Mill_Rep'), Customer=request.POST.get('Customer'), Marketing_Zone=request.POST.get('Marketing_Zone'), Status=1)
+            Mill=request.POST.get('Mill'), Date=request.POST.get('Date'), Mill_Rep=request.POST.get('Mill_Rep'), Customer=request.POST.get('Customer'), Marketing_Zone=request.POST.get('Marketing_Zone'), Status=vStatus)
     else:
 
         for item in data:
