bfe5730ddf54e09292821527a52003e5faab0491
MalikaWeeroda <malika.k@weeroda.com>
2022-06-03 17:45:55 +0530

    m1
    m1

--- RY_Enquiry_Form/models.py
+++ RY_Enquiry_Form/models.py
@@ -1,51 +1,56 @@
-from itertools import count
 from django.db import models
 
 
 class RY_Enquiry_Items(models.Model):
-    Counts = models.TextField(max_length=200)
-    Quality = models.TextField(max_length=200)
-    Type = models.TextField(max_length=200)
-    Blend = models.TextField(max_length=200)
-    Shade = models.TextField(max_length=200)
-    Shade_Ref = models.TextField(max_length=200)
-    Depth = models.TextField(max_length=200)
-    UOM = models.TextField(max_length=200)
-    Quantity = models.TextField(max_length=200)
-    Status = models.TextField(max_length=200)
-    Reg_no = models.IntegerField(max_length=200)
-    Id = models.IntegerField(max_length=200)
+    Id = models.IntegerField(max_length=200,  null=True)
+    Counts = models.TextField(max_length=200,  null=True)
+    Quality = models.TextField(max_length=200,  null=True)
+    Type = models.TextField(max_length=200,  null=True)
+    Blend = models.TextField(max_length=200,  null=True)
+    Shade = models.TextField(max_length=200,  null=True)
+    Shade_Ref = models.TextField(max_length=200,  null=True)
+    Depth = models.TextField(max_length=200,  null=True)
+    UOM = models.TextField(max_length=200, null=True)
+    Quantity = models.TextField(max_length=200,  null=True)
+    Status = models.TextField(max_length=200,  null=True)
+    Reg_no = models.TextField(max_length=200,  null=True)
+    Rate = models.ImageField(max_length=200,  null=True)
+    Amount = models.ImageField(max_length=200,  null=True)
+    Last_order = models.TextField(max_length=200,  null=True)
 
     class Meta:
         db_table = 'RY_Enquiry_Items'
         ordering = ['Reg_no']
 
     def __str__(self):
-        return self.Counts + ' ' + self.Quality + ' ' + self.Type + ' ' + self.Blend + ' ' + self.Shade + ' ' + self.Shade_Ref + ' ' + self.Depth + ' ' + self.UOM + ' ' + self.Quantity + ' ' + self.Reg_no + ' ' + self.Status + '' + self.Id
+        return self.Rate + ' ' + self.Amount + ' ' + self.Last_order + ' ' + self.Counts + ' ' + self.Quality + ' ' + self.Type + ' ' + self.Blend + ' ' + self.Shade + ' ' + self.Shade_Ref + ' ' + self.Depth + ' ' + self.UOM + ' ' + self.Quantity + ' ' + self.Reg_no + ' ' + self.Status + '' + self.Id
 
 
 class RY_Enquiry_Header(models.Model):
     # Id = models.IntegerField(max_length=200)
-    Reg_no = models.IntegerField(max_length=200)
-    Mill = models.TextField(max_length=200)
-    Mill_Rep = models.TextField(max_length=200)
-    Customer = models.TextField(max_length=200)
-    Marketing_Zone = models.TextField(max_length=200)
-    Payment_Term = models.TextField(max_length=200)
-    Narration = models.TextField(max_length=200)
-    Reason_For_Non_Acception = models.TextField(max_length=200)
-    Replied_From_the_mill = models.TextField(max_length=200)
-    Acceptance_from_the_mill = models.TextField(max_length=200)
-    Date = models.TextField(max_length=200)
-    Email_Details = models.TextField(max_length=200)
-    Status = models.TextField(max_length=200)
+    Reg_no = models.TextField(max_length=200,  null=True)
+    Mill = models.TextField(max_length=200,  null=True)
+    Mill_Rep = models.TextField(max_length=200, null=True)
+    Customer = models.TextField(max_length=200,  null=True)
+    Marketing_Zone = models.TextField(max_length=200,  null=True)
+    Payment_Term = models.TextField(max_length=200,  null=True)
+    Narration = models.TextField(max_length=200,  null=True)
+    Reason_For_Non_Acception = models.TextField(
+        max_length=200,  null=True)
+    Replied_From_the_mill = models.TextField(
+        max_length=200, null=True)
+    Acceptance_from_the_mill = models.TextField(
+        max_length=200,  null=True)
+    Date = models.TextField(max_length=200,  null=True)
+    Email_Details = models.TextField(max_length=200,  null=True)
+    Status = models.TextField(max_length=200, null=True)
 
     class Meta:
         db_table = 'RY_Enquiry_Header'
         ordering = ['Reg_no']
 
     def __str__(self):
-        return str(self.Reg_no) + ' ' + self.Mill + ' ' + self.Customer + ' ' + self.Marketing_Zone + ' ' + self.Payment_Term + ' ' + self.Narration + ' ' + self.Reason_For_Non_Acception + ' ' + self.Reason_For_Non_Acception + ' ' + self.Acceptance_from_the_mill + ' ' + self.Date + '' + self.Email_Details + ' ' + self.Status
+        return self.Mill_Rep + ' ' + self.Reg_no + ' ' + self.Mill + ' ' + self.Customer + ' ' + self.Marketing_Zone + ' ' + self.Payment_Term + ' ' + self.Narration + ' ' + self.Reason_For_Non_Acception + ' ' + self.Reason_For_Non_Acception + ' ' + self.Acceptance_from_the_mill + ' ' + self.Date + '' + self.Email_Details + ' ' + self.Status
 
 
 # class Email(models.Model):
