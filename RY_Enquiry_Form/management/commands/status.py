2677d1ab59c0a6d50c4f1a5b517d33e9e1dc789d
hariweeroda <hari@weeroda.com>
2022-08-20 19:23:31 +0530

    dummydata and status scripts
    dummydata and status scripts

--- RY_Enquiry_Form/management/commands/status.py
+++ RY_Enquiry_Form/management/commands/status.py
@@ -5,12 +5,29 @@ import sys
 
 mail = "logeshwari.m.k@weeroda.com"
 
+
 class Command(BaseCommand):
     help = "Status Update"
 
-    def handle(self, *args, **kwargs):
-        sts= '14'
-        reg= '10'
-        RY_Enquiry_Items.objects.filter(Reg_no=reg).update(Status=sts)
-        RY_Enquiry_Header.objects.filter(Reg_no=reg).update(Status=sts)
-        
+    def add_arguments(self, parser):
+
+        parser.add_argument('reg_no')
+        parser.add_argument('sts')
+
+        parser.add_argument("created_by", nargs='?', default=None)
+
+    def handle(self, *args, **options):
+
+        reg_no = options['reg_no']
+        sts = options['sts']
+
+        created_by = options['created_by']
+
+        if(created_by != None):
+            RY_Enquiry_Items.objects.filter(
+                Reg_no=reg_no).update(CreatedByUser=created_by)
+            RY_Enquiry_Header.objects.filter(
+                Reg_no=reg_no).update(CreatedByUser=created_by)
+
+        RY_Enquiry_Items.objects.filter(Reg_no=reg_no).update(Status=sts)
+        RY_Enquiry_Header.objects.filter(Reg_no=reg_no).update(Status=sts)
